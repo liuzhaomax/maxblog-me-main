1cc835314f22b7a747e3f1bc2d9537bf2f29a1d4
LIU Zhao <liuzhaomax@163.com>
2022-09-24 19:18:48 +1000

    lint

--- internal/middleware/interceptor/i_auth.go
+++ internal/middleware/interceptor/i_auth.go
@@ -3,9 +3,6 @@ package interceptor
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/wire"
-	"maxblog-me-main/internal/core"
-	"net/http"
-	"strings"
 	"sync"
 )
 
@@ -29,19 +26,19 @@ type Interceptor struct{}
 // emails in tokens need to be equal
 func (inter *Interceptor) CheckTwoTokens() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		// token in req header
-		headerToken := ctx.Request.Header.Get("Authorization")
-		headerToken, _ = core.RSADecrypt(core.GetPrivateKey(), headerToken)
-		headerTokenEmail, _ := core.ParseToken(headerToken)
-		headerTokenEmail = strings.Split(headerTokenEmail, "|")[0]
-		// token in req cookie
-		cookieToken, _ := ctx.Cookie("TOKEN")
-		cookieToken, _ = core.RSADecrypt(core.GetPrivateKey(), cookieToken)
-		cookieTokenEmail, _ := core.ParseToken(cookieToken)
-		// checking tokens info
-		if headerTokenEmail != cookieTokenEmail || headerTokenEmail == "" || cookieTokenEmail == "" {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, core.FormatError(200, nil))
-		}
+		//// token in req header
+		//headerToken := ctx.Request.Header.Get("Authorization")
+		//headerToken, _ = core.RSADecrypt(core.GetPrivateKey(), headerToken)
+		//headerTokenEmail, _ := core.ParseToken(headerToken)
+		//headerTokenEmail = strings.Split(headerTokenEmail, "|")[0]
+		//// token in req cookie
+		//cookieToken, _ := ctx.Cookie("TOKEN")
+		//cookieToken, _ = core.RSADecrypt(core.GetPrivateKey(), cookieToken)
+		//cookieTokenEmail, _ := core.ParseToken(cookieToken)
+		//// checking tokens info
+		//if headerTokenEmail != cookieTokenEmail || headerTokenEmail == "" || cookieTokenEmail == "" {
+		//	ctx.AbortWithStatusJSON(http.StatusUnauthorized, core.FormatError(200, nil))
+		//}
 		ctx.Next()
 	}
 }
