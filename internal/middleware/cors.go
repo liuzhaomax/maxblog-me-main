26c8cbb1d3ca19e0931f6db798f6a55fd5561f4f
LIU Zhao <liuzhaomax@163.com>
2022-09-26 21:33:41 +1000

    cors

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -10,7 +10,7 @@ import (
 func Cors() gin.HandlerFunc {
 	var corsWhiteList = []string{
 		fmt.Sprintf("http://%s", core.GetUpstreamAddr()),
-		fmt.Sprintf("http://%s", "34.129.133.50:9202"), // TODO for st env
+		fmt.Sprintf("http://%s", "127.0.0.1:3000"), // TODO for st env
 	}
 	return func(ctx *gin.Context) {
 		if core.In(corsWhiteList, ctx.Request.Header.Get("Origin")) {
